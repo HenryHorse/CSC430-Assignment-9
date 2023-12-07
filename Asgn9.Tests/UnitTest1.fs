0c77b18e0ba75be47feaa8d23455718d91b61eee
Shayan Daijavad <shayan@daijavad.com>
2023-12-06 21:31:19 -0800

    mostly done

--- Asgn9.Tests/UnitTest1.fs
+++ Asgn9.Tests/UnitTest1.fs
@@ -9,3 +9,27 @@ let Setup () =
 [<Test>]
 let Test1 () =
     Assert.IsTrue(true)
+
+
+
+// Takes an ExprC from the Asgn9 module and checks to see if calling interp with that ExprC returns the expected NumV
+[<Test>]
+let InterpTest1 () =
+    match interp (NumC 1) topEnv with
+    | NumV n -> Assert.AreEqual(1, n)
+    | _ -> Assert.Fail("interp did not return a NumV")
+[<Test>]
+let InterpTest2 () =
+    match interp (StrC "test") topEnv with
+    | StrV s -> Assert.AreEqual("test", s)
+    | _ -> Assert.Fail("interp did not return a StrV")
+[<Test>]
+let InterpTest3 () =
+    match interp (IdC "true") topEnv with
+    | BoolV v -> Assert.AreEqual(v, true)
+    | _ -> Assert.Fail("interp did not return a BoolV")
+[<Test>]
+let InterpTest4() =
+    match interp (IfC((IdC "true"), (NumC 1), (NumC 2))) topEnv with
+    | NumV n -> Assert.AreEqual(1, n)
+    | _ -> Assert.Fail("interp did not return a NumV")
--- Asgn9/Library.fs
+++ Asgn9/Library.fs
@@ -2,77 +2,102 @@
 
 open System
 
-
 type ExprC = 
-    | NumC of n: float
-    | IdC of s: string
-    | StrC of s: string 
+    | NumC of n : float
+    | IdC of s : string
+    | StrC of s : string
     | IfC of cond : ExprC * t : ExprC * e : ExprC
+    | AppC of f : ExprC * args : ExprC list
+    | LamC of parameters : string list * body : ExprC
 
-type Value =
+and Value =
     | NumV of value : float
     | BoolV of value : bool
     | StrV of value : string
-//    | ClosV of params : string list * body : ExprC * env : Env
-
-type PrimOpV =
+    | ClosV of parameters : string list * body : ExprC * env : Env
     | UnOpV of action : (Value -> Value)
     | BinOpV of action : (Value -> Value -> Value)
 
-//type Env = Binding list 
+and Binding = 
+    struct
+        val sym : string
+        val value : Value
+        new (s: string, v: Value) = { sym = s; value = v }
+    end
 
-//type Binding = 
- //    | sym  of value : string
- //    | binding of value : Value
+and Env = Binding list
 
-// 
-// let plus (x : Value) (y : Value) : Value =
-//   match x, y with
-//  | NumC a, NumC b -> NumV (a + b)
-//  | _ -> failwith "Arguments must be NumC values"
+let plus (x : Value) (y : Value) : Value =
+    match x, y with
+    | NumV a, NumV b -> NumV (a + b)
+    | _ -> failwith "Arguments must be NumC values"
 
-// let sub (x : Value) (y : Value) : Value =
-//   match x, y with
-//  | NumC a, NumC b -> NumV (a - b)
-//  | _ -> failwith "Arguments must be NumC values"
+let sub (x : Value) (y : Value) : Value =
+    match x, y with
+    | NumV a, NumV b -> NumV (a - b)
+    | _ -> failwith "Arguments must be NumC values"
 
-// let mult (x : Value) (y : Value) : Value =
-//   match x, y with
-//  | NumC a, NumC b -> NumV (a * b)
-//  | _ -> failwith "Arguments must be NumC values"
+let mult (x : Value) (y : Value) : Value =
+    match x, y with
+    | NumV a, NumV b -> NumV (a * b)
+    | _ -> failwith "Arguments must be NumC values"
 
-// let div (x : Value) (y : Value) : Value =
-//   match x, y with
-//  | NumC a, NumC b -> NumV (a / b)
-//  | _ -> failwith "Arguments must be NumC values"
+let div (x : Value) (y : Value) : Value =
+    match x, y with
+    | NumV a, NumV b -> NumV (a / b)
+    | _ -> failwith "Arguments must be NumC values"
 
-//let topEnv : Env =
-//    let initialBindings = [
-//      Binding (+) 
-//      Binding (*)
-//      Binding (-) 
-//      Binding (/) 
-//      Binding (true BoolV true)
-//      Binding (false BoolV false)
-//   ]
-//   initialBindings
+let lte (x : Value) (y : Value) : Value =
+    match x, y with
+    | NumV a, NumV b -> BoolV(a <= b)
+    | _ -> failwith "Arguments must be NumC values"
 
+let equal (x : Value) (y : Value) : Value =
+    match x, y with
+    | NumV a, NumV b -> BoolV(a = b)
+    | StrV a, StrV b -> BoolV(a = b)
+    | BoolV a, BoolV b -> BoolV(a = b)
+    | _ -> failwith "Arguments must be matching types"
 
-let extractNumVs (values: Value list) =
-    match values with
-    | [NumV x; NumV y] -> [x; y]
-    | _ -> failwith "Values are not NumVs"
+let topEnv : Env = [
+    new Binding("+", BinOpV plus);
+    new Binding("-", BinOpV sub);
+    new Binding("*", BinOpV mult);
+    new Binding("/", BinOpV div);
+    new Binding("<=", BinOpV lte);
+    new Binding("equal?", BinOpV equal);
+    new Binding("true", BoolV true);
+    new Binding("false", BoolV false);]
 
-// Uses a symbol 
+// Uses a symbol name to look up a value in an environment.
+let rec lookup (sym : string) (env : Env) : Value =
+    match env with
+    | [] -> failwith "Symbol not found"
+    | {sym = s; value = v} :: rest -> if s = sym then v else lookup sym rest
+
+// Calls the given function with the given arguments and environment.
+let rec call (f : Value) (args : Value list) (env : Env) : Value =
+    match f with
+    | UnOpV f -> f (args |> List.head)
+    | BinOpV f -> f (args |> List.head) (args |> List.tail |> List.head)
+    | ClosV (parameters, body, env) ->
+            // zip parameters and values to get a list of Binding structures
+            let bindings = List.zip parameters args |> List.map (fun (s, v) -> new Binding(s, v))
+            // interp body with new env
+            interp body (bindings @ env)
+    | _ -> failwith "Function is not a callable value"
 
 // Interprets an ExprC into a Value.
-let rec interp (e : ExprC) : Value =
+and interp (e : ExprC) (env : Env) : Value =
     match e with
     | NumC n -> NumV n
     | StrC s -> StrV s
-    | IdC s -> failwith "Not implemented"
+    | IdC s -> lookup s env
     | IfC (cond, t, e) ->
-        match (interp cond) with
-        | BoolV true -> interp t
-        | BoolV false -> interp e
+        match (interp cond env) with
+        | BoolV true -> interp t env
+        | BoolV false -> interp e env
         | _ -> failwith "If condition is not a boolean"
+    | AppC (f, args) -> call (interp f env) (List.map (fun arg -> interp arg env) args) env
+    | LamC (parameters, body) -> ClosV (parameters, body, env)
+
